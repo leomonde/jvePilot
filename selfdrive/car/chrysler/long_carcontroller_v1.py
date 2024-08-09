42d3e8b2793899162eb6598a436bec236c971576
John <4vanetten@gmail.com>
2024-08-09 09:56:34 -0600

    simulate hybrid more

--- selfdrive/car/chrysler/long_carcontroller_v1.py
+++ selfdrive/car/chrysler/long_carcontroller_v1.py
@@ -163,6 +163,7 @@ class LongCarControllerV1(LongCarController):
     can_sends.append(chryslercan.das_3_command(self.packer,
                                                2 if counter_das_3_changed else 3,
                                                go_req,
+                                               false if self.hybrid else torque is not None,
                                                1546.75 if self.hybrid else torque,
                                                self.max_gear,
                                                stop_req and not fidget_stopped_brake_frame,
