f9e5a78b69c36c80c18b673b7f3c27a0178b02dd
John <4vanetten@gmail.com>
2024-09-07 10:12:29 -0600

    jeeps

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -105,6 +105,10 @@ class CarControllerParams:
       self.STEER_DELTA_UP = 6
       self.STEER_DELTA_DOWN = 6
       self.STEER_MAX = 261  # EPS allows more, up to 350?
+    elif CP.carFingerprint in JEEPS:
+      self.STEER_DELTA_UP = 12
+      self.STEER_DELTA_DOWN = 12
+      self.STEER_MAX = 261  # EPS allows more, up to 350?
     else:
       self.STEER_DELTA_UP = 3
       self.STEER_DELTA_DOWN = 3
